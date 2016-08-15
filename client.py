1d5770082aa2f7e40af9545d9d47c2e792b82ce9
cheiffeast <minimoyle123@gmail.com>
2016-08-15 21:51:28 +0100

    Update client.py
    Added commenting and a windsound beep for new messages

--- client.py
+++ client.py
@@ -1,36 +1,47 @@
-import socket,pickle,os
+import socket,pickle,os,time
 from threading import Thread
-import time
+from winsound import Beep
+
+#Initalise the socket
 s = socket.socket()
+
+#connect to the sever
 s.connect(('localhost',20000))
+
+#Main function to fetch data from the server
 def receive():
 
     while True:
-        
+
+        #Wait to receive data from the server
         data = s.recv(8192)
+
+        #Deserialize the data from the server 
         data = pickle.loads(data)
         os.system("cls")
+
+        #Print out all the items in data
         for item in data:
             print(item)
 
-
+        #winsound.Beep when a new message is received
+        Beep(500,250)
+#Asking the user for their username
 username = input("Enter your username: ")
+
+#Create the thread to keep receiving data
 Thread(target=receive).start()
 
+#Send our username to the server
 s.send(username.encode())
 time.sleep(0.1)
 while True:
-    
+
+    #Ask the user for a message
     msg = input(">>")
 
-    if not msg:
+    if msg == "quit":
         break
+    
     s.send(msg.encode())
     time.sleep(0.1)
-##    data = s.recv(8192)
-##    data = pickle.loads(data)
-##    
-##    os.system("cls")
-##    
-##    for item in data:
-##        print(item)
